b6fc7dc7b7cd950cab8f982e1b54c6367718bce1
VIA2NEY <kouameangeV11@gmail.com>
2024-08-07 18:25:21 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/day_18_image_carrousel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.day_18_image_carrousel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:day_18_image_carrousel/screen/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  )
+);
+
